97af4fea2949dd7e203b18f3997cc056d8def308
Michelle Pomeroy <michelle.pomeroy@hotmail.com>
2013-02-11 21:26:55 -0500

    validations

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -5,4 +5,39 @@ class DiaryEntriesController < ApplicationController
 
 		@diary_entries_positive = DiaryEntry.positive
 	end
+
+	def new
+		@diary_entry = DiaryEntry.new
+	end
+
+	def create
+		@diary_entry = DiaryEntry.new params[:diary_entry]
+
+		if @diary_entry.save
+			redirect_to diary_entries_path
+		else
+			render :new
+		end
+
+	end
+
+	def edit
+		@diary_entry = DiaryEntry.find(params[:id])
+	end
+
+	def update
+		@diary_entry = DiaryEntry.find(params[:id])
+
+		if @diary_entry.update_attribues params[:diary_entry]
+			redirect_to diary_entries_path
+		else
+			render :edit
+		end
+
+	def destroy
+		@diary_entry = DiaryEntry.find(params[:id])
+		@diary_entry.destroy
+
+		redirect_to diary_entries_path
+	end
 end
--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -4,4 +4,13 @@ class DiaryEntry < ActiveRecord::Base
 	def self.positive
 		where(mood: "happy")
 	end
+
+	validates :description, presence: true
+	validates :title, presence: true, uniqueness: true
+	validates :mood_is_happy
+
+	def mood_is_happy
+		if Time.now.monday? || Time.now.thursday?
+		errors.add (:mood, "must be happy")
+	end
 end
